461480dc630c1f113d983478b6eaebb1604098b9
Darye <54469750+DaryeDev@users.noreply.github.com>
2022-07-28 04:52:23 +0200

    Loot's Tools Browser Companion

--- background.js
+++ background.js
@@ -1,93 +1,97 @@
-var domain = ""
-var SLTab = null
+var domain = "";
+var SLTab = null;
+var token = false;
+var userInfo = false;
 
-// chrome.browserAction.onClicked.addListener(function (tab) {
-//     injectToStreamloots()
-// });
-console.log
-function injectToStreamloots() {
-    chrome.tabs.query({
-        "active": true,
-        "currentWindow": true,
-        "status": "complete",
-        "windowType": "normal"
-    }, function (tabs) {
-        for (tab in tabs) {
-            domain = tabs[tab].url.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/)[1];
-            if (domain == "www.streamloots.com") {
-                SLTab = tabs[tab]
-                chrome.tabs.executeScript(tab.ib, {
-                    file: 'inject.js'
-                }, receiveText);
-                break;
-            } else {
-                alert("You need to use this Extension on a Streamloots' page.")
-            }
-                }
-    });
+
+function handleMessage(request, sender, sendResponse) {
+    if ("sendTokenToLootsTools" in request){
+        loginToLootsTools(request.sendTokenToLootsTools)
+    }
+    sendResponse({response: "Oído cocina!"});
 }
+function handleExternalMessage(request, sender, sendResponse) {
+    if ("registerDiscordServer" in request){
+        registerDiscordServer(request.registerDiscordServer)
+    }
+    sendResponse({response: "Oído cocina!"});
+}
+chrome.runtime.onMessage.addListener(handleMessage);
+chrome.runtime.onMessageExternal.addListener(handleExternalMessage);
 
 function getLang() {
-    if (navigator.languages != undefined) 
-        return navigator.languages[0]; 
+    if (navigator.languages != undefined) return navigator.languages[0];
     return navigator.language;
 }
 
-function receiveText(resultsArray){
-    token = resultsArray[0];
-    window.open("lootstools://", "_self");
-    if (token){
-        if (getLang().includes("es")){optionText = "Estás a punto de compartir tu token de Streamloots con Loot's Tools.\n\nEsto cede acceso a tu cuenta de Streamloots.\nSin embargo, Loot's Tools 𝘀𝗼𝗹𝗼 usará el token para iniciar sesión, conseguir información básica y llevar a cabo algunas acciones (Regalar cofres, por ejemplo) que 𝗲𝗹 𝘂𝘀𝘂𝗮𝗿𝗶𝗼 𝗱𝗲𝗰𝗶𝗱𝗮 con eventos (Cartas o Ventas de Cofres), y 𝗻𝘂𝗻𝗰𝗮 se usará o mandará a más que a Streamloots.\n\n¿Quieres continuar?"}
-        else {optionText = "You are about to share your Streamloots's Token with Loot's Tools.\n\nThis gives access to your Streamloots' Account. \nHowever, Loot's Tools will 𝗼𝗻𝗹𝘆 use the token lo log in, get basic information and execute some actions (Gift Packs, for example) that 𝘁𝗵𝗲 𝘂𝘀𝗲𝗿 𝘄𝗮𝗻𝘁𝘀 to be called with events (Card redeems and sold Packs), and will 𝗻𝗲𝘃𝗲𝗿 be used or sent to anyone but Streamloots.\n\nContinue?"}
-        if (confirm(optionText)) {
-            // chrome.tabs.update({url: "lootstools://login/"+token});
-            var tokenSent = false
-            var tries = 0
-            function websocketHandler() {
-                try {
-                    var socket = new WebSocket("ws://localhost:4848");
-                    window.socket = socket
-                    socket.onopen = function (e) {
-                        window.socket.send(`{"token":"${token}"}`);
-                        tokenSent = true
-                        socket.close()
-                        return
-                    };
-                    socket.onclose = function (event) {
-                        if (tokenSent == false){
-                            if (tries == 0) {
-                                tries += 1;
-                                chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
-                                    chrome.tabs.query({url: "https://lootstools.darye.dev/"}, function(tabs){
-                                        if (tabs.length == 0) {
-                                            chrome.tabs.update(tab.id, {url: "lootstools://init"});
-                                        } else {
-                                            chrome.tabs.update(tab.id, {url: "lootstools://"});
-                                        }
-                                    })
-                              });
-                              
-                            
-                            }
-                            window.setTimeout(websocketHandler, 500)
-                        }
-                    }
-                } catch (error) {
-                    
-                    window.setTimeout(websocketHandler, 10000)
-                }
-            }
-            websocketHandler()
+function registerDiscordServer(syncID) {
+    url = "finishSetupDiscordServer"
+    chrome.storage.sync.get(["token"], function (resulto) {
+        token = resulto.token;
+        var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
 
+        fetch("https://dysontools.herokuapp.com/finishSetupDiscordServer?syncID="+syncID+"&slt="+Base64.encode(token), {
+            method: 'GET',
+            headers: {},
+            mode: "no-cors"
+        })
+        .then((response) => function (response) {
+            console.log("sentRegistration")
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    });
+}
+
+function loginToLootsTools(token = false) {
+    if (token) {
+        var tokenSent = false;
+        var tries = 0;
+        function websocketHandler() {
+            try {
+                var socket = new WebSocket("ws://localhost:4848");
+                socket.onopen = function (e) {
+                    socket.send(`{"token":"${token}"}`);
+                    tokenSent = true;
+                    socket.close();
+                    return;
+                };
+                socket.onclose = function (event) {
+                    if (tokenSent == false) {
+                        if (tries == 0) {
+                            tries += 1;
+                            chrome.tabs.query(
+                                { currentWindow: true, active: true },
+                                function (tab) {
+                                    chrome.tabs.query(
+                                        { url: "https://lootstools.darye.dev/" },
+                                        function (tabs) {
+                                            if (tabs.length == 0) {
+                                                chrome.tabs.create({ url: "lootstools://" });
+                                            } else {
+                                                chrome.tabs.create({ url: "lootstools://init" });
+                                            }
+                                        }
+                                    );
+                                }
+                            );
+                        }
+                        chrome.alarms.create({ delayInMinutes: 0.00833333 });
+
+                        chrome.alarms.onAlarm.addListener(() => {
+                            websocketHandler()
+                        });
+                    }
+                };
+            } catch (error) {
+                chrome.alarms.create({ delayInMinutes: 0.1666667 });
+
+                chrome.alarms.onAlarm.addListener(() => {
+                    websocketHandler()
+                });
+            }
         }
-    }
-    else{
-        if (SLTab.url == "https://www.streamloots.com/collections"){
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                chrome.tabs.reload(tabs[0].id);window.setTimeout(injectToStreamloots(), 2000);
-            });
-        }
-        else{alert("Log in to Streamloots first (or reload the page if you did).")}
-        
+        websocketHandler();
     }
 }
