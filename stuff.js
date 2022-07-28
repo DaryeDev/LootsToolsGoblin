3258ef5a91aee905854a373dfa9bb3ae8b501aef
Darye <54469750+DaryeDev@users.noreply.github.com>
2022-07-28 23:19:16 +0200

    Added reloadUserInfoButton & syncStreamlootsTwitch

--- stuff.js
+++ stuff.js
@@ -8,6 +8,16 @@ document
     .addEventListener("click", injectToStreamloots);
 
 document
+    .getElementById("reloadUserInfoButton")
+    .addEventListener("click", function() {
+        document.getElementById("reloadUserInfoButtonIcon").className = "";
+        void document.getElementById("reloadUserInfoButtonIcon").offsetWidth
+        document.getElementById("reloadUserInfoButtonIcon").className = "spin";
+        chrome.storage.local.set({ userInfo: false }, function () { });
+        loadInfoToExtension(token, true);
+    });
+
+document
     .getElementById("loginLootsToolsButton")
     .addEventListener("click", loginToLootsTools);
 
@@ -23,7 +33,6 @@ document
         chrome.tabs.create({url:"lootstools://init"})
     });
 
-
 document.getElementById("logoutButton").addEventListener("click", function () {
     chrome.storage.sync.set({ token: false }, function () { });
     chrome.storage.local.set({ userInfo: false }, function () { });
@@ -31,9 +40,15 @@ document.getElementById("logoutButton").addEventListener("click", function () {
     document.getElementById("loggedIn").hidden = true;
     document.body.style.width = "";
 });
+
 document.getElementById("syncStreamlootsDiscordButton").addEventListener("click", function () {
     var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
     chrome.tabs.create({ url: "https://discord.com/oauth2/authorize?client_id=938205520781783050&redirect_uri=https%3A%2F%2Fdysontools.herokuapp.com%2FdiscordLogin&response_type=code&scope=identify&state=LTDSS"+Base64.encode(token) });
+});
+
+document.getElementById("syncStreamlootsTwitchButton").addEventListener("click", function () {
+    var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+    chrome.tabs.create({ url: "https://id.twitch.tv/oauth2/authorize?client_id=b7ybrqr5eoixm686m7or2a02dqdvci&redirect_uri=https%3A%2F%2Fdysontools.herokuapp.com%2FtwitchLogin&response_type=code&scope=user%3Aread%3Aemail&force_verify=true&state=LTTSS"+Base64.encode(token) });
 });
 
 function tryToLoadToken() {
@@ -113,6 +128,7 @@ function recieveToken(resultsArray) {
 
         $.ajax(settings).done(function (response) {
             chrome.storage.local.set({ userInfo: response }, function () { });
+            userInfo = response;
         });
 
         alert("Logged in!");
@@ -129,7 +145,7 @@ function recieveToken(resultsArray) {
     }
 }
 
-function loadInfoToExtension(token) {
+function loadInfoToExtension(token, forceReload=false) {
 
     function changeUserInfoInCompanion(userInfo) {
         document.getElementById("helloText").innerHTML = `Hey ${userInfo.profile.username}!`;
@@ -147,24 +163,30 @@ function loadInfoToExtension(token) {
 
         $.ajax(settings).done(function (response) {
             chrome.storage.local.set({ userInfo: response }, function () { });
+            userInfo = response;
             changeUserInfoInCompanion(userInfo)
         });
     }
 
-    chrome.storage.local.get(["userInfo"], function (resulto) {
-        if ($.isEmptyObject(resulto)){
-            userInfoFromStreamloots(token)
-        } else {
-            userInfo = resulto.userInfo;
-            if (userInfo){
-                changeUserInfoInCompanion(userInfo)
-            } else {
+    if (forceReload) {
+        userInfoFromStreamloots(token)
+    } else {
+        chrome.storage.local.get(["userInfo"], function (resulto) {
+            if ($.isEmptyObject(resulto)){
                 userInfoFromStreamloots(token)
+            } else {
+                userInfo = resulto.userInfo;
+                if (userInfo){
+                    changeUserInfoInCompanion(userInfo)
+                } else {
+                    userInfoFromStreamloots(token)
+                }
             }
-        }
-    });
+        });
+    }
 
-    document.body.style.width = "500px";
+    
+
     document.getElementById("notLoggedIn").hidden = true;
     document.getElementById("loggedIn").hidden = false;
 }
